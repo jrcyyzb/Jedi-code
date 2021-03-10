b2ad5caf24b0606c5e4313af15909cd12454a35e
hugh-98 <822051253@qq.com>
2021-03-10 23:24:11 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -19,11 +19,11 @@ def get_input(filename, data_path="./training-data"):
     :param data_path: 数据集所在文件夹路径
     :return:
         N:  可以采购的服务器类型数量, int
-        servers:  所有服务器类型信息, list.
-            [[第一种服务器的型号, CPU核数, 内存大小, 硬件成本, 每日能耗成本], [第二种...], ...]
+        servers:  所有服务器类型信息, dict.
+            {'第一种服务器类型型号':[cpu核数,内存大小,硬件成本,每日能耗成本], '第二种...':..., ...}
         M:  售卖的虚拟机类型数量, int
-        virtualMachines:  所有虚拟机类型信息, list
-            [[第一种虚拟机的型号, CPU核数, 内存大小, 是否双节点部署], [第二种...], ...]
+        virtualMachines:  所有虚拟机类型信息, dict
+            {'第一种虚拟机型号':[cpu核数,内存大小,是否双节点部署], '第二种...':...}
         T:  共有T天的用户请求序列, list
         requests:  所有的用户请求序列, list
             [   [[第一天第一条请求], [第一天第二条请求], ...],
@@ -36,15 +36,13 @@ def get_input(filename, data_path="./training-data"):
     # 可以采购的服务器类型数量
     N = int(details[0].rstrip("\n"))
     # 总服务器类型
-    servers = []
+    servers = {}
     # 遍历每种服务器
     for i in range(1, N + 1):
         server = []
         infomations_server = extract(details[i])
-
         # 服务器型号
         type = infomations_server[0].strip()
-        server.append(type)
         # cpu核数
         cpu = int(infomations_server[1].strip())
         server.append(cpu)
@@ -57,23 +55,23 @@ def get_input(filename, data_path="./training-data"):
         # 每日能耗成本
         cost_day = int(infomations_server[4].strip())
         server.append(cost_day)
-        servers.append(server)
+        servers[type] = server
+
     # 售卖的虚拟机类型数量
     M = int(details[N + 1].rstrip("\n"))
-    virtualMachines = []
+    virtualMachines = {}
     # 遍历每种虚拟机类型
     for i in range(N + 2, N + M + 2):
         vm = []
         infomations_vm = extract(details[i])
         type = infomations_vm[0].strip()
-        vm.append(type)
         cpu = int(infomations_vm[1].strip())
         vm.append(cpu)
         memory = int(infomations_vm[2].strip())
         vm.append(memory)
         isTwoNode = int(infomations_vm[3].strip())
         vm.append(isTwoNode)
-        virtualMachines.append(vm)
+        virtualMachines[type] = vm
     # T天的用户请求
     T = int(details[N + M + 2].rstrip("\n"))
     requests = []
@@ -102,4 +100,3 @@ def get_input(filename, data_path="./training-data"):
         requests.append(requests_day)
     # 返回服务器类型数量，每种服务器信息，每种虚拟机信息，总天数，总请求信息
     return N, servers, M, virtualMachines, T, requests
-
