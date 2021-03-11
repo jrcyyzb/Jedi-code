d9e273c67cea72a0e4874a2b41edcc097608e843
hugh-98 <822051253@qq.com>
2021-03-11 19:02:31 +0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 # @Author: Hugh
-# @Date: 2021/3/10 17:52
+# @Date: 2021/3/11 18:30
 # Software: PyCharm
-# 获取数据集中的数据
+# 获取输入数据
 
 
 # 将服务器/虚拟机数据格式转化为列表
-def extract(detail):
-    infomation = detail.rstrip("\n")
-    infomation = infomation[1:len(infomation)-1]
-    return infomation.split(",")
+def extract(info):
+    # infomation = detail.rstrip("\n")
+    info = info[1:len(info)-1]
+    return info.split(",")
 
 
-def get_input(filename, data_path="./training-data"):
+def get_input():
     '''
-    提取filename数据集中的输入
-    :param filename: 数据集名称
-    :param data_path: 数据集所在文件夹路径
+    输入
     :return:
         N:  可以采购的服务器类型数量, int
         servers:  所有服务器类型信息, dict.
@@ -31,16 +29,20 @@ def get_input(filename, data_path="./training-data"):
                 ...     ...
             ]
     '''
-    with open(data_path + "/" + filename, "r") as f:
-        details = f.readlines()
+    # 需要提交时把下三行代码注释即可
+    import sys
+    filename = "./training-data/training-1.txt"
+    # 将python标准输入input函数重定向为filename
+    sys.stdin = open(filename, 'r')
+
     # 可以采购的服务器类型数量
-    N = int(details[0].rstrip("\n"))
+    N = int(input())
     # 总服务器类型
     servers = {}
     # 遍历每种服务器
-    for i in range(1, N + 1):
+    for i in range(N):
         server = []
-        infomations_server = extract(details[i])
+        infomations_server = extract(input())
         # 服务器型号
         type = infomations_server[0].strip()
         # cpu核数
@@ -58,12 +60,12 @@ def get_input(filename, data_path="./training-data"):
         servers[type] = server
 
     # 售卖的虚拟机类型数量
-    M = int(details[N + 1].rstrip("\n"))
+    M = int(input())
     virtualMachines = {}
     # 遍历每种虚拟机类型
-    for i in range(N + 2, N + M + 2):
+    for i in range(M):
         vm = []
-        infomations_vm = extract(details[i])
+        infomations_vm = extract(input())
         type = infomations_vm[0].strip()
         cpu = int(infomations_vm[1].strip())
         vm.append(cpu)
@@ -73,18 +75,17 @@ def get_input(filename, data_path="./training-data"):
         vm.append(isTwoNode)
         virtualMachines[type] = vm
     # T天的用户请求
-    T = int(details[N + M + 2].rstrip("\n"))
+    T = int(input())
     requests = []
-    idx = N + M + 3
     for i in range(T):
         # 第i天的R条请求
-        R = int(details[idx].rstrip("\n"))
+        R = int(input())
         # 一天的请求
         requests_day = []
         # 第i天的第j条请求
         for j in range(R):
             request = []
-            req = extract(details[idx+j+1])
+            req = extract(input())
             opration = req[0].strip()
             request.append(opration)
             if opration=="add":
@@ -96,7 +97,6 @@ def get_input(filename, data_path="./training-data"):
                 id = int(req[1].strip())
                 request.append(id)
             requests_day.append(request)
-        idx += R + 1
         requests.append(requests_day)
     # 返回服务器类型数量，每种服务器信息，每种虚拟机信息，总天数，总请求信息
     return N, servers, M, virtualMachines, T, requests
