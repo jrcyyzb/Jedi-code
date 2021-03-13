6ca79c81a58714a6723e5fb6dca5547e6ce92b54
hugh-98 <822051253@qq.com>
2021-03-13 18:42:20 +0800

    Update cal_cost.py

--- cal_cost.py
+++ cal_cost.py
@@ -2,7 +2,7 @@ import get_data
 
 servers_Num, servers_Dict, vm_Num, vm_Dict, Days, req_Sqe = get_data.get_input()
 
-# hardwareSum = 0
+hardwareSum = 0
 
 
 def cal_cost(purchase_dict, servers_Pool):
@@ -11,14 +11,17 @@ def cal_cost(purchase_dict, servers_Pool):
 
     # 硬件成本
     for key, values in purchase_dict.items():
-        hardwareSum += servers_Dict[key][2]
-        #print("硬件成本: ", hardwareSum)
+        hardwareSum += servers_Dict[key][2] * values
+        # print("硬件成本: ", hardwareSum)
 
     # 每天能耗成本
     for i in range(len(servers_Pool)):
-        if servers_Pool[i][1] or servers_Pool[i][2] or servers_Pool[i][3] or servers_Pool[i][4]:
-            name = servers_Pool[i][0]  # 服务器型号
-            hardwareSum += servers_Dict[name][2]
-            #print("每天能耗成本: ", hardwareSum)
+        name = servers_Pool[i][0]  # 服务器型号
+        cpu = servers_Dict[name][0] / 2
+        nc = servers_Dict[name][1] / 2
+        # print(cpu, nc)
+        if servers_Pool[i][1] != cpu or servers_Pool[i][3] != cpu or servers_Pool[i][2] != nc or servers_Pool[i][4] != nc:
+            hardwareSum += servers_Dict[name][3]
+            # print("每天能耗成本: ", hardwareSum)
 
     return hardwareSum
